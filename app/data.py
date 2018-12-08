6ec6f3f781435b1aae47bc84a42f683579f627be
masterpiece00 <mikhailRomanovVI@gmail.com>
2018-12-09 01:16:10 +0300

    Added project

---
+++ app/data.py
@@ -0,0 +1,26 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Article One',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet non iure error. Hic ex magnam in ullam, tempore quidem nemo perferendis, laudantium unde id fuga nam accusamus possimus omnis aspernatur.',
+			'author': 'Brad Traversy',
+			'create_date': '04-25-2017'
+		},
+		{
+			'id': 2,
+			'title': 'Article Two',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet non iure error. Hic ex magnam in ullam, tempore quidem nemo perferendis, laudantium unde id fuga nam accusamus possimus omnis aspernatur.',
+			'author': 'Jhon Doe',
+			'create_date': '04-25-2017'
+		},
+		{
+			'id': 3,
+			'title': 'Article Three',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet non iure error. Hic ex magnam in ullam, tempore quidem nemo perferendis, laudantium unde id fuga nam accusamus possimus omnis aspernatur.',
+			'author': 'Brad Traversy',
+			'create_date': '04-25-2017'
+		}
+	]
+
+	return articles
